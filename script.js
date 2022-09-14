57dc2e8443abe632cea454bbaaad74d4170de2ae
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-14 23:00:53 +0200

    Complete mob nav

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ import generateCarCard from './utils/carCard.js'
 
 generateServiceCard()
 generateCarCard()
+
+
+const mobNav = document.querySelector('.mob-nav-container')
+const navBars = document.querySelector('.nav-bars')
+const navBtns = document.querySelector('.nav-btns')
+const close = document.createElement('a')
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
+close.setAttribute('class', 'close text-decoration-none')
+close.innerHTML = 'X'
+
+navBars.addEventListener('click', () => {
+    navBtns.appendChild(close)
+    mobNav.appendChild(navBtns)
+})
+
+close.addEventListener('click', () => {
+    mobNav.removeChild(navBtns)
+})
