2095880931c55bfd0c5f69c2c3920c9c547a16de
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-16 12:11:35 +0200

    Fix ESlint Errors

--- other-scripts/about.js
+++ other-scripts/about.js
@@ -1,20 +1,18 @@
-const mobNav = document.querySelector('.mob-nav-container')
-const desctopNav = document.querySelector('.nav-items')
-const navBars = document.querySelector('.nav-bars')
-const navBtns = document.querySelector('.nav-btns')
-const close = document.createElement('a')
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const mobNav = document.querySelector('.mob-nav-container');
+const desctopNav = document.querySelector('.nav-items');
+const navBars = document.querySelector('.nav-bars');
+const navBtns = document.querySelector('.nav-btns');
+const close = document.createElement('a');
 
-close.setAttribute('class', 'close text-decoration-none')
-close.innerHTML = 'X'
+close.setAttribute('class', 'close text-decoration-none');
+close.innerHTML = 'X';
 
 navBars.addEventListener('click', () => {
-    navBtns.appendChild(close)
-    mobNav.appendChild(navBtns)
-})
+  navBtns.appendChild(close);
+  mobNav.appendChild(navBtns);
+});
 
 close.addEventListener('click', () => {
-    mobNav.removeChild(navBtns)
-    desctopNav.appendChild(navBtns)
-
-})
+  mobNav.removeChild(navBtns);
+  desctopNav.appendChild(navBtns);
+});
--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-import generateServiceCard from './utils/serviceCard.js'
-import generateCarCard from './utils/carCard.js'
+import generateServiceCard from './utils/serviceCard.js';
+import generateCarCard from './utils/carCard.js';
 
-generateServiceCard()
-generateCarCard()
+generateServiceCard();
+generateCarCard();
 
+const mobNav = document.querySelector('.mob-nav-container');
+const desctopNav = document.querySelector('.nav-items');
+const navBars = document.querySelector('.nav-bars');
+const navBtns = document.querySelector('.nav-btns');
+const close = document.createElement('a');
 
-const mobNav = document.querySelector('.mob-nav-container')
-const desctopNav = document.querySelector('.nav-items')
-const navBars = document.querySelector('.nav-bars')
-const navBtns = document.querySelector('.nav-btns')
-const close = document.createElement('a')
-
-close.setAttribute('class', 'close text-decoration-none')
-close.innerHTML = 'X'
+close.setAttribute('class', 'close text-decoration-none');
+close.innerHTML = 'X';
 
 navBars.addEventListener('click', () => {
-    navBtns.appendChild(close)
-    mobNav.appendChild(navBtns)
-})
+  navBtns.appendChild(close);
+  mobNav.appendChild(navBtns);
+});
 
 close.addEventListener('click', () => {
-    mobNav.removeChild(navBtns)
-    desctopNav.appendChild(navBtns)
-
-})
+  mobNav.removeChild(navBtns);
+  desctopNav.appendChild(navBtns);
+});
