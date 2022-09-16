c43f4de9e7afad06c4b67bd6827fceb35ce8a353
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-16 14:00:55 +0200

    Fix bug in assets #4

--- data/services.js
+++ data/services.js
@@ -1,26 +1,26 @@
 const services = [
   {
-    logo: '../assets/icons/car-seat.png',
+    logo: 'assets/icons/car-seat.png',
     serviceTitle: 'Comfort',
     serviceDescription: 'travel anywhere with high comfort, less amount of money and friendly driver',
   },
   {
-    logo: '../assets/icons/fast.png',
+    logo: 'assets/icons/fast.png',
     serviceTitle: 'Fast',
     serviceDescription: 'travel anywhere with high comfort, less amount of money and friendly driver',
   },
   {
-    logo: '../assets/icons/time.png',
+    logo: 'assets/icons/time.png',
     serviceTitle: 'Time Dedication',
     serviceDescription: 'travel anywhere with high comfort, less amount of money and friendly driver',
   },
   {
-    logo: '../assets/icons/friendly.png',
+    logo: 'assets/icons/friendly.png',
     serviceTitle: 'Friendly',
     serviceDescription: 'travel anywhere with high comfort, less amount of money and friendly driver',
   },
   {
-    logo: '../assets/icons/tech.png',
+    logo: 'assets/icons/tech.png',
     serviceTitle: 'Technology',
     serviceDescription: 'travel anywhere with high comfort, less amount of money and friendly driver',
   },
